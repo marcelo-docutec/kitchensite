fac092f23a3d8863b280c3e573eda4e7dcea6ff1
sanson222 <molinasergio91@gmail.com>
2019-11-30 23:28:53 -0300

    add effect for foodCards

--- src/components/Food/index.js
+++ src/components/Food/index.js
@@ -1,18 +1,20 @@
-import React from 'react';
-import { Card, CardActionArea, CardContent, Typography, CardMedia } from '@material-ui/core';
+import React, { useState} from 'react';
+import { CardActionArea, CardContent, Typography, CardMedia } from '@material-ui/core';
+import Card from './styles';
 
 const Food = ({ image, description }) => {
+  const [raised, setRaised] = useState(false);
   return (
-    <Card>
+    <Card.Styled raised={raised} onMouseOut={() => setRaised(false)} onMouseOver={() => setRaised(true)}>
       <CardActionArea>
-        <CardMedia image={ `./${image}` } component="img" />
+        <CardMedia image={`./${image}`} component="img" />
         <CardContent>
           <Typography variant="body2" color="textSecondary" component="p">
             {description}
           </Typography>
         </CardContent>
       </CardActionArea>
-    </Card>
+    </Card.Styled>
   );
 };
 
---
+++ src/components/Food/styles.js
@@ -0,0 +1,10 @@
+import { styled } from '@material-ui/core';
+import { Card as CardBase } from '@material-ui/core';
+
+const Styled = styled(CardBase)({
+  overflow: 'visible',
+  marginBottom: '15px',
+  borderRadius: 0,
+});
+
+export default { Styled };
