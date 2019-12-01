2b53838503223f9fb0b40c6c645c2113bd978d3a
MarceloLudovino <marcelo.docutec@gmail.com>
2019-11-30 22:04:29 -0300

    Add drawer

--- src/App.js
+++ src/App.js
@@ -2,17 +2,18 @@ import React from 'react';
 import Home from './pages/Home';
 import { Provider } from 'react-redux';
 import store from './core/store';
-import Navbar from 'components/Navbar';
 import { ThemeProvider } from '@material-ui/styles';
 import { theme } from 'core/theme';
+import ResponsiveDrawer from 'components/ResponsiveDrawer';
 
 const App = () => {
   return (
     <div className="App">
       <Provider store={store}>
         <ThemeProvider theme={theme}>
-          <Navbar />
-          <Home />
+          <ResponsiveDrawer>
+            <Home />
+          </ResponsiveDrawer>
         </ThemeProvider>
       </Provider>
     </div>
